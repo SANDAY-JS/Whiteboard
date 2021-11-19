df60cb0910a1661e51ff6836c0c421a355758ec3
SANDAY-JS <san.sh.s@i.softbank.jp>
2021-11-19 17:29:16 +0900

    Initial Commit

---
+++ WhiteBoard.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteBoard : MonoBehaviour
+{
+    public Texture2D texture;
+
+    public Vector2 textureSize = new Vector2(x:2048, y: 2048)
+
+    void Start()
+    {
+        var r = GetComponent<Renderer>();
+        texture = new Texture2D((int)textureSize.x, (int)textureSize.y)
+        e.material.mainTexture = texture
+    }
+}
