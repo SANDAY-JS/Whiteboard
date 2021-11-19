844774731a554cd7deb2ac0f10777b36386214ac
SANDAY-JS <san.sh.s@i.softbank.jp>
2021-11-19 21:28:47 +0900

    Error Fixing

--- WhiteBoard.cs
+++ WhiteBoard.cs
@@ -6,12 +6,12 @@ public class WhiteBoard : MonoBehaviour
 {
     public Texture2D texture;
 
-    public Vector2 textureSize = new Vector2(x:2048, y: 2048)
+    public Vector2 textureSize = new Vector2(x: 2048, y: 2048);
 
     void Start()
     {
         var r = GetComponent<Renderer>();
-        texture = new Texture2D((int)textureSize.x, (int)textureSize.y)
-        e.material.mainTexture = texture
+        texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
+        e.material.mainTexture = texture;
     }
 }
